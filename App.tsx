010c850e65802b4a45ada3e005cded75eefd88a2
yankydoo <yankydoo@users.noreply.github.com>
2022-11-30 22:51:49 +0100

    reset

--- App.tsx
+++ App.tsx
@@ -2,31 +2,15 @@ import * as React from 'react';
 import { fetchMessage } from './api/MessagesApi';
 import './style.css';
 
-const useMessage = (x: number) => {
-  const [message, setMessage] = React.useState('');
-
-  React.useEffect(() => {
-    const abortController = new AbortController();
-    fetchMessage(x, abortController).then((message) => {
-      setMessage(message);
-    });
-    return () => abortController.abort();
-  }, [x]);
-  return message;
-};
-
 export default function App() {
-  const [x, setX] = React.useState(0);
-  const message = useMessage(x);
-
+  let x = 0
   return (
     <div>
-      <h1>Hello Florian!</h1>
+      <h1>Hello you!</h1>
       <p>The x is {x}.</p>
-      <p>My message is {message}</p>
       <button
         onClick={() => {
-          setX(x + 1);
+          x += 1;
         }}
       >
         Increase
